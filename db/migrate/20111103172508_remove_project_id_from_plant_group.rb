c58f7a1f13d95a942cd99b0e9cf7da16624182f2
mmelone12 <matt.m.melone@gmail.com>
2011-12-17 16:33:09 +0100

    fixing heroku migration issues

--- db/migrate/20111103172508_remove_project_id_from_plant_group.rb
+++
@@ -1,9 +0,0 @@
-class RemoveProjectIdFromPlantGroup < ActiveRecord::Migration
-  def self.up
-    remove_column :plant_groups, :project_id
-  end
-
-  def self.down
-    add_column :plant_groups, :project_id, :integer
-  end
-end
